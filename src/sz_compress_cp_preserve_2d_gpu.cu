78f8ea728cef1a2980a4d920034a2dc296874ed4
bzhan166 <bzhan166@ucr.edu>
2025-03-18 16:05:56 -0400

    delete branch to examine

--- src/sz_compress_cp_preserve_2d_gpu.cu
+++ src/sz_compress_cp_preserve_2d_gpu.cu
@@ -150,7 +150,7 @@ template<typename T>
     //T det = u0v1 - u1v0 + u1v2 - u2v1 + u2v0 - u0v2;
     T det = U0V1 - U1V0 + U1V2 - U2V1 + U2V0 - U0V2;
     T eb = 0;
-    if(det != 0)
+    //if(det != 0)
     {   
         //T d1 = u2v0 - u0v2;
         //T d2 = u1v2 - u2v1;
@@ -161,18 +161,22 @@ template<typename T>
         bool f1 = (det / d1 >= T(1));
         bool f2 = (det / d2 >= T(1));
         bool f3 = (det / d3 >= T(1)); 
-        if(!f1)
+        //if(!f1)
         {
             // T eb_cur = gpu_minf(gpu_max_eb_to_keep_sign_2d_offline_2_degree2(U2V0, -U0V2), gpu_max_eb_to_keep_sign_2d_offline_4_degree2(U0V1, -U1V0, U1V2, -U2V1));
             
-            T pos1 = (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
-            T neg1 = (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
+            // T pos1 = (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
+            // T neg1 = (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
+            T pos1 = U2V0;
+            T neg1 = -U0V2;
             T P1 = sqrt(pos1);
             T N1 = sqrt(neg1);
             T res1 = fabs(P1 - N1) / (P1 + N1);
 
-            T pos2 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0) + (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0);
-            T neg2 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0) + (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0);
+            // T pos2 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0) + (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0);
+            // T neg2 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0) + (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0);
+            T pos2 = U0V1 + U1V2;
+            T neg2 = -U1V0 - U2V1;
             T P2 = sqrt(pos2);
             T N2 = sqrt(neg2);
             T res2 = fabs(P2 - N2) / (P2 + N2);
@@ -180,43 +184,43 @@ template<typename T>
 
             eb = MAX(eb, eb_cur);
         }
-        if(!f2)
-        {
-            // T eb_cur = gpu_minf(gpu_max_eb_to_keep_sign_2d_offline_2_degree2(U1V2, -U2V1), gpu_max_eb_to_keep_sign_2d_offline_4_degree2(U0V1, -U1V0, U2V0, -U0V2));
+        // if(!f2)
+        // {
+        //     // T eb_cur = gpu_minf(gpu_max_eb_to_keep_sign_2d_offline_2_degree2(U1V2, -U2V1), gpu_max_eb_to_keep_sign_2d_offline_4_degree2(U0V1, -U1V0, U2V0, -U0V2));
             
-            T pos1 = (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0);
-            T neg1 = (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0);
-            T P1 = sqrt(pos1);
-            T N1 = sqrt(neg1);
-            T res1 = fabs(P1 - N1) / (P1 + N1);
+        //     T pos1 = (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0);
+        //     T neg1 = (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0);
+        //     T P1 = sqrt(pos1);
+        //     T N1 = sqrt(neg1);
+        //     T res1 = fabs(P1 - N1) / (P1 + N1);
 
-            T pos2 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0) + (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
-            T neg2 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0) + (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
-            T P2 = sqrt(pos2);
-            T N2 = sqrt(neg2);
-            T res2 = fabs(P2 - N2) / (P2 + N2);
-            T eb_cur = gpu_minf(res1, res2);
+        //     T pos2 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0) + (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
+        //     T neg2 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0) + (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
+        //     T P2 = sqrt(pos2);
+        //     T N2 = sqrt(neg2);
+        //     T res2 = fabs(P2 - N2) / (P2 + N2);
+        //     T eb_cur = gpu_minf(res1, res2);
             
-            eb = MAX(eb, eb_cur);
-        }
-        if(!f3)
-        {
-            // T eb_cur = gpu_minf(gpu_max_eb_to_keep_sign_2d_offline_2_degree2(U0V1, -U1V0), gpu_max_eb_to_keep_sign_2d_offline_4_degree2(U1V2, -U2V1, U2V0, -U0V2));
-            T pos1 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0);
-            T neg1 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0);
-            T P1 = sqrt(pos1);
-            T N1 = sqrt(neg1);
-            T res1 = fabs(P1 - N1) / (P1 + N1);
+        //     eb = MAX(eb, eb_cur);
+        // }
+        // if(!f3)
+        // {
+        //     // T eb_cur = gpu_minf(gpu_max_eb_to_keep_sign_2d_offline_2_degree2(U0V1, -U1V0), gpu_max_eb_to_keep_sign_2d_offline_4_degree2(U1V2, -U2V1, U2V0, -U0V2));
+        //     T pos1 = (U0V1 >= 0 ? U0V1 : 0) + ((-U1V0) >= 0 ? (-U1V0) : 0);
+        //     T neg1 = (U0V1 < 0 ? -U0V1 : 0) + ((-U1V0) < 0 ? U1V0 : 0);
+        //     T P1 = sqrt(pos1);
+        //     T N1 = sqrt(neg1);
+        //     T res1 = fabs(P1 - N1) / (P1 + N1);
 
-            T pos2 = (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0) + (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
-            T neg2 = (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0) + (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
-            T P2 = sqrt(pos2);
-            T N2 = sqrt(neg2);
-            T res2 = fabs(P2 - N2) / (P2 + N2);
-            T eb_cur = gpu_minf(res1, res2);
+        //     T pos2 = (U1V2 >= 0 ? U1V2 : 0) + ((-U2V1) >= 0 ? (-U2V1) : 0) + (U2V0 >= 0 ? U2V0 : 0) + ((-U0V2) >= 0 ? (-U0V2) : 0);
+        //     T neg2 = (U1V2 < 0 ? -U1V2 : 0) + ((-U2V1) < 0 ? U2V1 : 0) + (U2V0 < 0 ? -U2V0 : 0) + ((-U0V2) < 0 ? U0V2 : 0);
+        //     T P2 = sqrt(pos2);
+        //     T N2 = sqrt(neg2);
+        //     T res2 = fabs(P2 - N2) / (P2 + N2);
+        //     T eb_cur = gpu_minf(res1, res2);
 
-            eb = MAX(eb, eb_cur);
-        }
+        //     eb = MAX(eb, eb_cur);
+        // }
         eb = gpu_minf(eb, 1);
     }
     return eb;
@@ -343,6 +347,7 @@ __global__ void derive_eb_offline_v3(const T* __restrict__ dU, const T* __restri
 #define localCol threadIdx.x
 
     // load data from global memory to shared memory
+    #pragma unroll
     for (int i = 0; i < YSEQ; i++)
         {
         if(row < r1 && col < r2){
@@ -353,6 +358,7 @@ __global__ void derive_eb_offline_v3(const T* __restrict__ dU, const T* __restri
     __syncthreads();
 
     //BOTTLENECK IS HERE
+    # pragma unroll
     for (int i = 0; i < YSEQ; i++)
     {
         if(localRow<YSEQ*TileDim_Y-1 && localCol<TileDim_X-1){
@@ -362,6 +368,7 @@ __global__ void derive_eb_offline_v3(const T* __restrict__ dU, const T* __restri
         
     }
     __syncthreads();
+    return;
 
     T localmin;
     for (int i = 0; i < YSEQ; i++)
@@ -794,6 +801,7 @@ sz_compress_cp_preserve_2d_offline_gpu(const T * U, const T * V, size_t r1, size
     printf("maxdiff: %f, maxdiff_index: %d, error count: %d\n", maxdiff, maxdiff_index, count);
     printf("eb_v_gpu: %f, eb_v: %f\n", eb_v_gpu[maxdiff_index], eb_v[maxdiff_index]);
     
+    return 0;
 
     // deal with eb =zero  
     //U
