6f402ecd1aed150ac4df84c67600d3e59a5cdbde
bzhan166 <bzhan166@ucr.edu>
2026-04-14 08:50:09 -0400

    updated Readme

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 Critical point preserving compression for vector fields (GPU Implementation)
 
 # Installation
+Intall cpu verison cpsz <br>
+Dependancies: ZSTD <br>
+sh build_script.sh <br>
+Install gpu verison cpsz <br> 
 cd src <br>
 make <br>
-Dependancies: ZSTD
 
 # Run compression
 ./cpszg_2d ../data/uf.dat ../data/vf.dat 2400 3600 0.1 <br>
@@ -12,4 +15,4 @@ Decompressed data files are data/uf.dat.out and data/vf.dat.out
 
 # Evaluation
 cd .. <br>
-./external/ftk/build/bin/ex_cp_2d 3600 2400 ./data/uf.dat ./data/vf.dat <br>
+./build/build/bin/cp_extraction_2d data/uf.dat data/vf.dat 3600 2400 <br>
--- build_script.sh
+++ build_script.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 src_dir=`pwd`
 
+# build FTK for evaluation
+mkdir -p ${src_dir}/external
+cd ${src_dir}/external
+git clone https://github.com/lxAltria/ftk.git
+cd ftk
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${src_dir}/external/ftk/install ..
+make -j 4
+make install
+
 # build cpSZ
 cd ${src_dir}
 mkdir build
 cd build
 cmake ..
 make -j 4
-
-# build FTK for evaluation
-mkdir -p ${src_dir}/external
-cd ${src_dir}/external
-git clone https://github.com/lxAltria/ftk.git
-cd ftk
-#git checkout eval-cpSZ
-#git reset --hard 78c1e80aa71b52f852eacfdbcea44129e62f3641 
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${src_dir}/external/ftk/install ..
-make -j 4
-make install
